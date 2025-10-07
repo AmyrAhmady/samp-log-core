36f7f53b8c3d9ec80dee039ea755e8df954c7267
iAmir <hhm6@yahoo.com>
2025-10-07 13:56:38 +0330

    remove deprecated functions

--- include/samplog/PluginLogger.hpp
+++ include/samplog/PluginLogger.hpp
@@ -33,23 +33,9 @@ namespace samplog
 			return _logger->Log(level, msg);
 		}
 
-		inline bool Log(LogLevel level, const char *msg,
-			std::vector<AmxFuncCallInfo> const &call_info)
-		{
-			return _logger->Log(level, msg, call_info);
-		}
-
 		inline bool Log(AMX * const amx, const LogLevel level, const char *msg)
 		{
-			std::vector<AmxFuncCallInfo> call_info;
-			return Api::Get()->GetAmxFunctionCallTrace(amx, call_info)
-				&& _logger->Log(level, msg, call_info);
-		}
-
-		inline bool LogNativeCall(AMX * const amx, cell * const params,
-			const char *name, const char *params_format)
-		{
-			return _logger->LogNativeCall(amx, params, name, params_format);
+			return _logger->Log(level, msg);
 		}
 
 		inline bool operator()(LogLevel level, const char *msg)
@@ -60,12 +46,6 @@ namespace samplog
 		inline bool operator()(AMX * const amx, const LogLevel level, const char *msg)
 		{
 			return Log(amx, level, msg);
-		}
-
-		inline bool operator()(AMX * const amx, cell * const params,
-			const char *name, const char *params_format)
-		{
-			return LogNativeCall(amx, params, name, params_format);
 		}
 	};
 
